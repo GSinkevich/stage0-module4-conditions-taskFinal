767aac60587a1cd944428b64d23766608efd6b6c
DESKTOP-GV962H6\sinke <sinkevich.work@gmail.com>
2022-11-13 23:37:07 +0300

    task done

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        if (monthNumber <= 0 || monthNumber > 12){
+            System.out.println("Wrong month number");
+            return;
+        }
+        switch (monthNumber){
+            case 12,1,2:
+                System.out.println("Winter");
+                break;
+            case 3,4,5:
+                System.out.println("Autumn");
+                break;
+            case 6,7,8:
+                System.out.println("Summer");
+                break;
+            case 9,10,11:
+                System.out.println("Spring");
+                break;
+
+        }
+
     }
 }
